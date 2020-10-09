73c6019e2f4b7bddcf70e1f2b98605d433c41701
Noam Preil <noam@pixelhero.dev>
2020-10-09 18:46:32 -0400

    Minor sdccconf cleanup; version bump

--- src/sdccconf.h
+++ src/sdccconf.h
@@ -9,25 +9,13 @@
 #ifndef SDCCCONF_HEADER
 #define SDCCCONF_HEADER
 
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
-
-/* XXX */
 #define BIN2DATA_DIR DIR_SEPARATOR_STRING "../share"
-
-/* XXX */
 #define BINDIR "/usr/local/bin"
 
-/* XXX */
 #define DATADIR "/usr/local/share"
-
-/* XXX */
 #define DIR_SEPARATOR_CHAR '/'
 
-/* XXX */
 #define DIR_SEPARATOR_STRING "/"
-
-/* XXX */
 #define EXEC_PREFIX "/usr/local"
 
 /* Define to 1 if you have the <boost/graph/adjacency_list.hpp> header file.
@@ -96,226 +84,75 @@
 
 /* Define to 1 if you have the `vsprintf' function. */
 #define HAVE_VSPRINTF 1
-
-/* XXX */
 #define INCLUDE_DIR_SUFFIX DIR_SEPARATOR_STRING "sdcc/include"
-
-/* XXX */
 #define LIB_DIR_SUFFIX DIR_SEPARATOR_STRING "sdcc/lib"
-
-/* XXX */
 #define NON_FREE_INCLUDE_DIR_SUFFIX DIR_SEPARATOR_STRING "sdcc/non-free/include"
-
-/* XXX */
 #define NON_FREE_LIB_DIR_SUFFIX DIR_SEPARATOR_STRING "sdcc/non-free/lib"
 
-/* Define to 1 to disable the AVR port */
 #define OPT_DISABLE_AVR 1
-
-/* XXX */
 #define OPT_DISABLE_DEVICE_LIB 1
-
-/* XXX */
 #define OPT_DISABLE_DS390 1
-
-/* XXX */
 #define OPT_DISABLE_DS400 1
-
-/* XXX */
 #define OPT_DISABLE_GBZ80 1
-
-/* XXX */
 #define OPT_DISABLE_HC08 1
-
-/* XXX */
 #define OPT_DISABLE_MCS51 1
-
-/* XXX */
 #define OPT_DISABLE_NON_FREE 1
-
-/* XXX */
 #define OPT_DISABLE_PACKIHX 0
-
-/* XXX */
 #define OPT_DISABLE_PIC14 1
-
-/* XXX */
 #define OPT_DISABLE_PIC16 1
-
-/* XXX */
 #define OPT_DISABLE_R2K 1
-
-/* XXX */
 #define OPT_DISABLE_R3KA 1
-
-/* XXX */
 #define OPT_DISABLE_S08 1
-
-/* XXX */
-#define OPT_DISABLE_SDBINUTILS 0
-
-/* XXX */
+#define OPT_DISABLE_SDBINUTILS 1
 #define OPT_DISABLE_SDCDB 1
-
-/* XXX */
 #define OPT_DISABLE_SDCPP 1
-
-/* XXX */
 #define OPT_DISABLE_STM8 1
-
-/* XXX */
 #define OPT_DISABLE_TININative 1
-
-/* XXX */
 #define OPT_DISABLE_TLCS90 1
-
-/* XXX */
 #define OPT_DISABLE_UCSIM 1
-
-/* Define to 1 to disable the XA51 port */
 #define OPT_DISABLE_XA51 1
-
-/* XXX */
 #define OPT_DISABLE_Z180 1
-
-/* XXX */
 #define OPT_DISABLE_Z80 0
-
-/* XXX */
 #define OPT_ENABLE_DOC 0
-
-/* XXX */
 #define OPT_ENABLE_LIBGC 0
 
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME ""
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION ""
-
-/* XXX */
 #define PREFIX "/usr/local"
-
-/* XXX */
 #define PREFIX2BIN_DIR DIR_SEPARATOR_STRING "bin"
-
-/* XXX */
 #define PREFIX2DATA_DIR DIR_SEPARATOR_STRING "share"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
-
-/* XXX */
-#define SDCC_DIR_NAME "SDCC_HOME"
-
-/* XXX */
-#define SDCC_INCLUDE_NAME "SDCC_INCLUDE"
-
-/* XXX */
-#define SDCC_LIB_NAME "SDCC_LIB"
-
-/* XXX */
-#define SDCC_VERSION_HI 3
-
-/* XXX */
-#define SDCC_VERSION_LO 4
-
-/* XXX */
-#define SDCC_VERSION_P 1
-
-/* XXX */
-#define SDCC_VERSION_STR "3.4.1"
+#define SDCC_DIR_NAME "KCC_HOME"
+#define SDCC_INCLUDE_NAME "KCC_INCLUDE"
+#define SDCC_LIB_NAME "KCC_LIB"
+#define SDCC_VERSION_HI 4
+#define SDCC_VERSION_LO 0
+#define SDCC_VERSION_P 2
+#define SDCC_VERSION_STR "4.0.2"
 
 /* The size of `char', as computed by sizeof. */
 #define SIZEOF_CHAR 1
-
 /* The size of `int', as computed by sizeof. */
 #define SIZEOF_INT 4
-
 /* The size of `long', as computed by sizeof. */
 #define SIZEOF_LONG 8
-
 /* The size of `long long', as computed by sizeof. */
 #define SIZEOF_LONG_LONG 8
-
 /* The size of `short', as computed by sizeof. */
 #define SIZEOF_SHORT 2
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* XXX */
-#define STD_DS390_LIB "libds390"
-
-/* XXX */
-#define STD_DS400_LIB "libds400"
-
-/* XXX */
-#define STD_FP_LIB "libfloat"
-
-/* XXX */
-#define STD_INT_LIB "libint"
-
-/* XXX */
-#define STD_LIB "libsdcc"
-
-/* XXX */
-#define STD_LONG_LIB "liblong"
-
-/* XXX */
 #define TYPE_BYTE char
-
-/* XXX */
 #define TYPE_DWORD int
-
-/* XXX */
 #define TYPE_QWORD long
-
-/* XXX */
 #define TYPE_UBYTE unsigned char
-
-/* XXX */
 #define TYPE_UDWORD unsigned int
-
-/* XXX */
 #define TYPE_UQWORD unsigned long
-
-/* XXX */
 #define TYPE_UWORD unsigned short
-
-/* XXX */
 #define TYPE_WORD short
 
 /* Define to 1 if c supports tags in unnamed struct. */
 #define UNNAMED_STRUCT_TAG 1
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
-# endif
-#endif
-
-/* Define to 1 if type `char' is unsigned and you are not using gcc.  */
-#ifndef __CHAR_UNSIGNED__
-/* # undef __CHAR_UNSIGNED__ */
-#endif
 
 #endif /* SDCCCONF_HEADER */
